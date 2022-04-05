ef167885b9816b7b6d85c9216fa7bef21c9fd374
Shehroz Sohail <75964325+shehroz218@users.noreply.github.com>
2022-04-05 09:21:21 -0700

    Add files via upload

--- vehicle-vision/app.py
+++ vehicle-vision/app.py
@@ -1,11 +1,11 @@
-from cProfile import label
-from msilib.schema import CheckBox
-import os
-import json
-from tkinter import Button
-from xml.etree.ElementTree import VERSION
-from nbformat import write
-import requests
+# from cProfile import label
+# from msilib.schema import CheckBox
+# import os
+# import json
+# # from tkinter import Button
+# from xml.etree.ElementTree import VERSION
+# from nbformat import write
+# import requests
 import SessionState
 import streamlit as st
 import tensorflow as tf
@@ -87,4 +87,4 @@ if pred_button:
 if session_state.pred_button:
     session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
     st.write(f"Prediction: {session_state.pred_class}, \
-               Confidence: {session_state.pred_conf:.3f}%")
+               Confidence: {session_state.pred_conf:.2f}%")
