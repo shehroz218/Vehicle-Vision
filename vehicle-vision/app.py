0fce99af5678604f7587ae4478bd33c8e00c9dc0
Shehroz Sohail <75964325+shehroz218@users.noreply.github.com>
2022-04-05 09:13:34 -0700

    Add files via upload
    using new h5 format and trained on the efficient net models instead of the less accurate mobile net model

--- vehicle-vision/app.py
+++ vehicle-vision/app.py
@@ -1,35 +1,52 @@
 from cProfile import label
-# from msilib.schema import CheckBox
+from msilib.schema import CheckBox
 import os
 import json
-# from tkinter import Button
+from tkinter import Button
 from xml.etree.ElementTree import VERSION
 from nbformat import write
 import requests
 import SessionState
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenet import preprocess_input
-tf.keras.mixed_precision.set_global_policy('mixed_float16')
-from utils import load_and_prep_image, classes_and_models, predict_json
+from tensorflow.keras.applications.efficientnet import preprocess_input
 
-# Environment credentials for accessing google cloud account
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\Codes\Image-Classification\shehroz-dl-playground-218-4cc90568b002.json" # change for your GCP key
-PROJECT = "shehroz-dl-playground-218" 
-REGION = "us-central1" 
-
-
-
-
-
-### Streamlit code (works as a straigtht-forward script) ###
 st.title("Welcome to Vehicle Vision 🚗📸")
 st.header("Identify what type of vehicle is in the picture!")
 
+base_classes = ['Ambulance',
+    'Barge',
+    'Bicycle',
+    'Boat',
+    'Bus',
+    'Car',
+    'Cart',
+    'Caterpillar',
+    'Helicopter',
+    'Limousine',
+    'Motorcycle',
+    'Segway',
+    'Snowmobile',
+    'Tank',
+    'Taxi',
+    'Truck',
+    'Van']
 
+# Model Choice
+choose_model = st.sidebar.selectbox(
+    "Pick model you'd like to use",
+    ("Model 1: EffecientNetB3","Model 2: EfficientNetB5")
+)
 
+if choose_model == "Model 1: EffecientNetB3":
+    CLASSES = base_classes
+    MODEL = tf.keras.models.load_model("D:\Codes\Image-Classification\model_EfficientNetB5.h5")
 
-@st.cache
+if choose_model == "Model 2: EfficientNetB5":
+    CLASSES = base_classes
+    MODEL = tf.keras.models.load_model("model_EfficientNetB5")
+
+# @st.cache
 def make_prediction(image, model, class_names):
     """
     Takes an image and uses model (a trained TensorFlow model) to make a
@@ -39,39 +56,19 @@ def make_prediction(image, model, class_names):
      pred_class (prediction class from class_names)
      pred_conf (model confidence)
     """
+    img = tf.image.decode_jpeg(image)
+    img = tf.image.resize(img, [224,224])
+    img=preprocess_input(img)
+    img= tf.cast(tf.expand_dims(img, axis=0), tf.int16)
+    prediction=model.predict(img)
 
+    pred_class=class_names[tf.argmax(prediction[0])]
+    pred_prob=(tf.reduce_max(prediction)*100).numpy()
+    return image,pred_class,pred_prob
 
-    
-    image = load_and_prep_image(image)
-    
-    prediction = predict_json(project=PROJECT,
-                         region=REGION,
-                         model=model,
-                         instances=image)
-    pred_class = class_names[tf.argmax(prediction[0])]
-    pred_conf = tf.reduce_max(prediction[0])*100
-    return image, pred_class, pred_conf
-
-
-# Model Choice
-choose_model = st.sidebar.selectbox(
-    "Pick model you'd like to use",
-    ("Model 1","Model 2")
-)
-
-if choose_model == "Model 1":
-    CLASSES = classes_and_models["model_1"]["classes"]
-    MODEL = classes_and_models["model_1"]["model_name"]
-
-#Display info about chosen model
-if st.checkbox('Show Model Classes'):
-    st.write(f"You chose {MODEL}, these are the classes of vehicle it can identify:\n", CLASSES)
-
-# Upload image to run model on
+#Upload File
 uploaded_file=st.file_uploader(label="Upload an Image of a Vehicle", type=["png","jpeg","jpg"])
-
-
-
+#Start session State so that site does not load on every interaction
 session_state = SessionState.get(pred_button=False)
 
 # Create logic for app flow
@@ -86,13 +83,8 @@ else:
 # Did the user press the predict button?
 if pred_button:
     session_state.pred_button = True 
-
-# And if they did...
+# Make Prediction
 if session_state.pred_button:
     session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
     st.write(f"Prediction: {session_state.pred_class}, \
                Confidence: {session_state.pred_conf:.3f}%")
-
-
-
-
